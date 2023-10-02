d34012ad01e798c2ef878f4bb9a947319f49b574
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2023-10-02 17:10:18 +0530

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Linkerlogo from './logo.svg'
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [willTrack,setTrack] = useState('notracking');
+  function handleChange(event){
+    setTrack(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <div className='ImageWrapper'>
+          <p>Share Data to Linker!!</p>
+          <image src={Linkerlogo} className='LinkerLogo' alt="Linker logo"/>
+        </div>
+        <label>
+          <input type="radio" value="track" checked={willTrack === 'track'} onChange={handleChange} />
+            Allow Sending Data
+        </label>
+        <label>
+          <input type="radio" value="notracking" checked={willTrack === 'notracking'} onChange={handleChange} />
+            Hide Content from Linker
+        </label>
+        
+      </React.Fragment>
     </div>
   );
 }
